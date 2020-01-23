06c7796c7fa88d98d7cd6108741dbffd36ba1e08
Michelle Cui <michelle@michelle-CuideMacBook-Pro.local>
2020-01-23 21:15:18 +1300

    Updates

---
+++ precache-manifest.9f4f8496fc5aa9cf540e93e5a159f8d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99076e3b17c2ae3364207118c868aa9c",
+    "url": "/monster-dex/index.html"
+  },
+  {
+    "revision": "0266c165c17d3d4862a0",
+    "url": "/monster-dex/static/css/main.5e3a4d15.chunk.css"
+  },
+  {
+    "revision": "1093a6240466003a5984",
+    "url": "/monster-dex/static/js/2.3a82cee6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-dex/static/js/2.3a82cee6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0266c165c17d3d4862a0",
+    "url": "/monster-dex/static/js/main.601fcf6c.chunk.js"
+  },
+  {
+    "revision": "97d11a1f6b6f67e4373e",
+    "url": "/monster-dex/static/js/runtime-main.7dc50925.js"
+  }
+]);
